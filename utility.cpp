5b864f88e8cf49dfb8ad26340320a7bdfff4ab36
Koissi Adjorlolo <yotcho2@gmail.com>
2016-12-08 21:50:43 -0800

    Added utility files

---
+++ utility.cpp
@@ -0,0 +1,11 @@
+#include "utility.h"
+
+#include <math.h>
+
+bool AABBi::intersects(AABBi o)
+{
+    return center.x < o.center.x + o.size.x &&
+        center.x + size.x > o.center.x &&
+        center.y < o.center.y + o.size.y &&
+        size.y + center.y > o.center.y;
+}
---
+++ utility.h
@@ -0,0 +1,26 @@
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
+
+typedef struct Vector2i
+{
+    Vector2i() : x(0), y(0) {}
+    Vector2i(int x, int y) : x(x), y(y) {}
+    int x, y;
+} Vector2i;
+
+typedef struct AABBi
+{
+    AABBi() : center(Vector2i()), size(Vector2i()) {}
+    Vector2i center, size;
+    bool intersects(AABBi other);
+} AABBi;
+
+typedef struct Color
+{
+    Color() : r(0), g(0), b(0) {}
+    Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+    uint8_t r, g, b;
+} Color;
+
+
+#endif /* __UTILITY_H__ */
