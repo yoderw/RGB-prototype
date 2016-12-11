cafac43d6935cb2deb5f1b040ff1aabdb0d5b864
Koissi Adjorlolo <yotcho2@gmail.com>
2016-12-11 11:49:37 -0800

    Somehow got object-object collision working

--- gameplay.cpp
+++ gameplay.cpp
@@ -30,8 +30,40 @@ static int STATIC_LEVEL[STATIC_LEVEL_H][STATIC_LEVEL_W] = {
     { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
     { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 };
+static int STATIC_LEVEL_MASK[STATIC_LEVEL_H][STATIC_LEVEL_W] = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+};
 
-static const double DEFAULT_TILES_PER_SECOND = 0.2;
+static CollisionResponse *COLLISION_MATRIX[ObjectType_Count][ObjectType_Count] = {
+    { new CollisionResponse(false, NULL), new CollisionResponse(false, NULL), new CollisionResponse(false, NULL) },
+    { new CollisionResponse(false, NULL), new CollisionResponse(false, NULL), new CollisionResponse(true, NULL) },
+    { new CollisionResponse(false, NULL), new CollisionResponse(true, NULL), new CollisionResponse(false, NULL) },
+};
+
+static const double DEFAULT_TILES_PER_SECOND = 0.08;
 
 void Object::setSize(Vector2i size)
 {
@@ -53,6 +85,11 @@ Vector2i Object::getTilePos()
     return _tilePos;
 }
 
+void Object::resetNextTilePos()
+{
+    _nextTilePos = _tilePos;
+}
+
 Vector2i Object::getNextTilePos()
 {
     return _nextTilePos;
@@ -61,6 +98,16 @@ Vector2i Object::getNextTilePos()
 double Object::getInterpolation()
 {
     return _interpolation;
+}
+
+bool Object::isInterpolating()
+{
+    return _interpolating;
+}
+
+ObjectType Object::getType()
+{
+    return _type;
 }
 
 void Object::init()
@@ -121,10 +168,10 @@ void TestObject::init()
 void TestObject::update(double dt)
 {
     Object::update(dt);
-    if (controller.left) _goLeft();
-    if (controller.right) _goRight();
-    if (controller.up) _goUp();
-    if (controller.down) _goDown();
+    if (controller.left && !(controller.down || controller.up)) _goLeft();
+    if (controller.right && !(controller.down || controller.up)) _goRight();
+    if (controller.up && !(controller.left || controller.right)) _goUp();
+    if (controller.down && !(controller.left || controller.right)) _goDown();
 }
 
 void TestObject::deinit()
@@ -132,18 +179,45 @@ void TestObject::deinit()
     Object::deinit();
 }
 
+void TestCrate::init()
+{
+    color = Color(0, 255, 0);
+}
+
+void TestCrate::update(double dt)
+{
+}
+
+void TestCrate::deinit()
+{
+}
+
 Level::Level()
 {
     _size = Vector2i(STATIC_LEVEL_W, STATIC_LEVEL_H);
     _levelData = (int **)malloc(sizeof(int *) * _size.y);
+    _maskData = (int **)malloc(sizeof(int *) * _size.y);
     for (int y = 0; y < _size.y; y++)
     {
         _levelData[y] = (int *)malloc(sizeof(int) * _size.x);
+        _maskData[y] = (int *)malloc(sizeof(int) * _size.x);
         for (int x = 0; x < _size.x; x++)
         {
             _levelData[y][x] = STATIC_LEVEL[y][x];
+            _maskData[y][x] = STATIC_LEVEL_MASK[y][x];
         }
     }
+}
+
+Level::~Level()
+{
+    for (int y = 0; y < _size.y; y++)
+    {
+        free(_levelData[y]);
+        free(_maskData[y]);
+    }
+    free(_levelData);
+    free(_maskData);
 }
 
 void Level::init()
@@ -157,6 +231,42 @@ void Level::update(double dt)
         Object *object = objects[objectIndex];
         if (object->playable) Events::updateController(object->controller);
         object->update(dt);
+
+        if (getMaskAtIndex(object->getNextTilePos()))
+        {
+            object->resetNextTilePos();
+        }
+    }
+
+    for (int objectIndex1 = 0; objectIndex1 < objects.size(); objectIndex1++)
+    {
+        for (int objectIndex2 = objectIndex1; objectIndex2 < objects.size(); objectIndex2++)
+        {
+            Object *object1 = objects[objectIndex1];
+            Object *object2 = objects[objectIndex2];
+            if (object1 == object2) continue;
+            ObjectType type1 = object1->getType();
+            ObjectType type2 = object2->getType();
+            CollisionResponse *response = COLLISION_MATRIX[type1][type2];
+            if (response)
+            {
+                if (response->shouldCollide)
+                {
+                    if (object1->getNextTilePos().equals(object2->getTilePos()))
+                    {
+                        object1->resetNextTilePos();
+                    }
+                    if (object2->getNextTilePos().equals(object1->getTilePos()))
+                    {
+                        object2->resetNextTilePos();
+                    }
+                }
+                if (response->callback)
+                {
+                    //response->callback(object1, object2);
+                }
+            }
+        }
     }
 }
 
@@ -181,6 +291,12 @@ int Level::getTileAtIndex(Vector2i index)
     return _levelData[index.y][index.x];
 }
 
+int Level::getMaskAtIndex(Vector2i index)
+{
+    if (index.x >= _size.x || index.y < 0 || index.y >= _size.y || index.y < 0) return -1;
+    return _maskData[index.y][index.x];
+}
+
 Vector2i Level::getSize()
 {
     return _size;
@@ -191,6 +307,8 @@ void TestLevel::init()
     Level::init();
     TestObject *playerObject = new TestObject(Vector2i(TILE_SIZE, TILE_SIZE), Vector2i(10, 10));
     objects.push_back(playerObject);
+    TestCrate *crateObject = new TestCrate();
+    objects.push_back(crateObject);
 
     for (int objectIndex = 0; objectIndex < objects.size(); objectIndex++)
     {
--- gameplay.h
+++ gameplay.h
@@ -7,10 +7,18 @@
 #include "utility.h"
 #include "events.h"
 
+typedef enum ObjectType
+{
+    ObjectType_None = 0,
+    ObjectType_Player = 1,
+    ObjectType_Crate = 2,
+    ObjectType_Count = 3,
+} ObjectType;
+
 typedef struct Object
 {
-    Object(Vector2i size, Vector2i tilePos) : _size(size), _tilePos(tilePos), _nextTilePos(tilePos), _interpolating(false), _interpolation(0.0), _interpolationTimer(0.0) {}
-    Object() : Object(Vector2i(), Vector2i()) {}
+    Object(Vector2i size, Vector2i tilePos, ObjectType type) : _size(size), _tilePos(tilePos), _nextTilePos(tilePos), _interpolating(false), _interpolation(0.0), _interpolationTimer(0.0), _type(type) {}
+    Object() : Object(Vector2i(), Vector2i(), ObjectType_None) {}
     Controller controller;
     Color color;
     bool playable;
@@ -18,8 +26,11 @@ typedef struct Object
     Vector2i getSize();
     void setTilePos(Vector2i tilePos);
     Vector2i getTilePos();
+    void resetNextTilePos();
     Vector2i getNextTilePos();
     double getInterpolation();
+    bool isInterpolating();
+    ObjectType getType();
     virtual void init();
     virtual void update(double dt);
     virtual void deinit();
@@ -29,6 +40,7 @@ typedef struct Object
         void _goUp();
         void _goDown();
     private:
+        ObjectType _type;
         Vector2i _size;
         Vector2i _tilePos;
         Vector2i _nextTilePos;
@@ -39,15 +51,24 @@ typedef struct Object
 
 typedef struct TestObject : public Object
 {
-    TestObject(Vector2i size, Vector2i tilePos) : Object(size, tilePos) {}
+    TestObject(Vector2i size, Vector2i tilePos) : Object(size, tilePos, ObjectType_Player) {}
     virtual void init();
     virtual void update(double dt);
     virtual void deinit();
 } TestObject;
 
+typedef struct TestCrate : public Object
+{
+    TestCrate() : Object(Vector2i(32, 32), Vector2i(5, 5), ObjectType_Crate) {}
+    virtual void init();
+    virtual void update(double dt);
+    virtual void deinit();
+} TestCrate;
+
 typedef struct Level
 {
     Level();
+    ~Level();
     std::vector<Object *> objects;
     virtual void init();
     virtual void update(double dt);
@@ -55,10 +76,12 @@ typedef struct Level
     Object getObjectAtIndex(size_t index);
     size_t nObjects();
     int getTileAtIndex(Vector2i index);
+    int getMaskAtIndex(Vector2i index);
     Vector2i getSize();
     private:
         Vector2i _size;
         int **_levelData;
+        int **_maskData;
 } Level;
 
 typedef struct TestLevel : public Level
@@ -67,5 +90,14 @@ typedef struct TestLevel : public Level
     virtual void update(double dt);
     virtual void deinit();
 } TestLevel;
+
+typedef void (*CollisionCallback)(Object *o1, Object *o2);
+
+typedef struct CollisionResponse
+{
+    CollisionResponse(bool shouldCollide, CollisionCallback callback) : shouldCollide(shouldCollide), callback(callback) {}
+    CollisionCallback callback;
+    bool shouldCollide;
+} CollisionResponse;
 
 #endif /* __GAMEPLAY_H__ */
